41783960d46c7f98ed3db49127fd604972972d40
SAVEEN-J <saveenj1996@gmail.com>
2024-01-23 22:36:29 +0530

    curd

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@
 import axios from 'axios';
 
 import React, { useEffect, useRef, useState } from 'react';
+import CreateNote from './components/CreateNote';
+import { Route, Router, Routes } from 'react-router';
+import ReadNotes from './components/ReadNotes';
+import { Link } from 'react-router-dom';
+import Home from './components/Home';
 
-function Note({note}) {
-  return (
-    <li>{ note.content }</li>
-  )
-}
 
 function App() {
   
@@ -30,30 +30,21 @@ function App() {
 
   // create a reference for the first input text box
   const newNoteContentRef = useRef(null);
-
-
   // define the addNote method
   let addNote = (event) => {
     event.preventDefault();
-    
-    // create a new object
+      // create a new object
     let noteObject = {
       id: notes.length + 1,
       content: newNoteContent,
       important: newNoteImportant==='true',
     }
-
     // add the new object to the notes state
     // setNotes(notes.concat(noteObject));
-
     //send thbe record to json usinf post method
-
     axios
     .post('http://localhost:3002/notes', noteObject)
     // .then(response => console.log(response))
-
-    // console.log(noteObject);
-
     // clear the input text box
     setNewNoteContent('');
     setNewNoteImportant('');
@@ -70,73 +61,24 @@ function App() {
 
   let handleStatusChange = (event) => {
     setShowStatus(event.target.value);
-    // console.log(event.target.value);
-  }
-
-  let filterNotes = (notes, status) => {
-    switch (status) {
-      case 'all':
-        return notes;
-      case 'imp':
-        return notes.filter(note => note.important === true);
-      case 'nonimp':
-        return notes.filter(note => note.important === false);
-    }
-  }
-
-  const notesFiltered = filterNotes(notes, showStatus);
+     }
 
   return (
-    <div>
-      <h1>Notes</h1>
+ <Router>
+  <div>
+    <Link to="/">Home </Link>
+    <Link to="/read">Read Notes</Link>
+    <Link to="/create">Create Notes</Link>
 
-      <label>
-        <input type='radio' name='filter' value="all" checked={ showStatus === 'all' } onChange={handleStatusChange} />
-        Show All Notes
-      </label>
 
-      <label>
-        <input type='radio' name='filter' value="imp" checked={ showStatus === 'imp' } onChange={handleStatusChange} />
-        Show Important Notes
-      </label>
-
-      <label>
-        <input type='radio' name='filter' value="nonimp" checked={ showStatus === 'nonimp' } onChange={handleStatusChange} />
-        Show Non-Important Notes
-      </label>
-
-      <ul>
-        {notesFiltered.map(note => 
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
-
-      {/* add a simple form for adding notes */}
-      <form onSubmit={addNote}>
-        <input
-          value={newNoteContent}
-          onChange={handleNoteChange}
-          placeholder='type a note...' 
-          ref={newNoteContentRef}
-          /> <br /><br />
-        {/* <input
-          type='text'
-          placeholder='enter true or false'
-          value={newNoteImportant}
-          onChange={(e) => setNewNoteImportant(e.target.value)} /> */}
-        <label form='dropdownNoteImportant'>Is Important: </label>
-        <select
-          id='dropdownNoteImportant'
-          onChange={handleSelectChange}
-          value={newNoteImportant}>
-          <option>--Select--</option>
-          <option>true</option>
-          <option>false</option>
-        </select>
-        <br /><br />
-        <button type='submit'>Add New Note</button>
-      </form>
-    </div>
+  </div>
+    <Routes>
+      <Route path='/read' element={<ReadNotes showStatus={showStatus} handleStatusChange={handleStatusChange} notes={notes}/>} />
+      <Route path='/create' element={ <CreateNote addNote={addNote} newNoteContent={newNoteContent} handleNoteChange={handleNoteChange} newNoteContentRef={newNoteContentRef} handleSelectChange={handleSelectChange} newNoteImportant={newNoteImportant} />} />
+      <Route path='/' element={<Home />} />
+    </Routes>
+</Router>
+    
   )
 }
 
---
+++ src/components/Note.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Note({note}) {
+    return (
+      <li>{ note.content }</li>
+    )
+  }
+export default Note;
